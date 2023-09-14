88cb1c054eddf872c0786eb65f8542977e49229e
st123067 <st123067@student.spbu.ru>
2023-09-15 01:05:03 +0300

    Lesson 1 code update

--- 2023.09.07-Homework01/project05/project05.cpp
+++ 2023.09.07-Homework01/project05/project05.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 	int a = 0;
 	int b = 0;
 	std::cin >> a;
-	std:: cin >> b;
+	std::cin >> b;
 	std::cout << (a * b % 109 + 109) % 109 + 1 << std::endl;
 	return EXIT_SUCCESS;
 }
--- 2023.09.07-Homework01/project06/project07.cpp
+++ 2023.09.07-Homework01/project06/project07.cpp
@@ -5,7 +5,7 @@ int main(int argc,char* argv[])
 {
     int a = 0;
     std::cin >> a;
-    std::cout << "The next number for the number" << " " << a << " " << "is" << " " << a + 1 << std::endl;
-    std::cout << "The previous number for the number" << " " << a << " " << "is" << " " << a - 1 << std::endl;
+    std::cout << "The next number for the number" << " " << a << " " << "is" << " " << a + 1 << "." << std::endl;
+    std::cout << "The previous number for the number" << " " << a << " " << "is" << " " << a - 1 << "." << std::endl;
     return EXIT_SUCCESS;
 }
