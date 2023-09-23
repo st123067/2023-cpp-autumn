46a010cce214d860abb0076f50aafdf7acb8ef61
st123067 <st123067@student.spbu.ru>
2023-09-23 11:59:49 +0300

    Lesson 2 code upload

---
+++ 2023.09.22-Homework02/Project02/Project02/Project02.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int a = 0;
+    int b = 0;
+    std::cin >> a;
+    std::cin >> b;
+    if ((a == 1) && (b ==1))  
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else if ((a != 1) && (b != 1))
+    {
+        std::cout << "YES" << std::endl;
+    }
+    else
+    {
+        std::cout << "NO" << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
+
---
+++ 2023.09.22-Homework02/Project03/Project03.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    int x = 0;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+    std::cin >> d;
+    if ((a == 0) && (b == 0))
+    {
+        std::cout << "INF" << std::endl;
+    }
+    else if (abs(b) % a == 0)
+    {
+        x = -b / a;
+        std::cout << x << std::endl;
+    }
+    else if ((a == 0) || (b * c == a * d))
+    {
+        std::cout << "NO" << std::endl;
+    }
+    else
+    {
+        std::cout << "NO" << std::endl;
+    }
+    return EXIT_SUCCESS;
+}
+
